754c79bd538eed44b77aa8fc8eb1d3abd49aca43
WS <ws@pmbs.de>
2022-11-08 21:49:44 +0100

    Programm erstellt

---
+++ winfriedEclipse/Programm/JaKo2022/jako2022/src/main/java/jako/jako2022/App.java
@@ -0,0 +1,13 @@
+package jako.jako2022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
